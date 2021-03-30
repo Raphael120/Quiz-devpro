2b35d96e44248b8b62fae332eaf434789a215848
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2021-03-30 17:42:19 -0300

    quiz_devpro

---
+++ quiz/base/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+
+from quiz.base.models import Aluno
+
+
+class AlunoForm(ModelForm):
+    class Meta:
+        model = Aluno
+        fields = ['nome', 'email']
+        
